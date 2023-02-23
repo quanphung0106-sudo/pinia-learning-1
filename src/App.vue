4a718b3993216e4c956837276ba75d35fe124ad5
quanphung0106-sudo <phungdinhquan16@gmail.com>
2023-02-23 14:43:29 +0700

    ninja: buoi 4

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+
+    <div class="task-list">
+      <p>All tasks</p>
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list">
+      <p>Fav tasks</p>
+      <div v-for="task in taskStore.favs">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import TaskDetails from './components/TaskDetails.vue'
+import { useTaskStore } from './stores/TaskStore'
+export default {
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTaskStore()
+    return { taskStore }
+  }
+}
+</script>
---
+++ src/stores/TaskStore.js
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+
+export const useTaskStore = defineStore("taskStore", {
+  state: () => ({
+    tasks: [
+      { id: 1, title: "buy some milk", isFav: false },
+      { id: 2, title: "play Gloomhaven", isFav: true },
+    ],
+  }),
+  getters: {
+    favs() {
+      return this.tasks.filter((task) => task.isFav);
+    },
+  },
+});
